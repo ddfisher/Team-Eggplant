da746968d4a7954983f9e305ff74c47437ea1a65
Lucas Baker <Onizuka@dn0a208ae4.sunet>
2010-04-26 15:32:06 -0700

    added evaluators (not yet implemented)

---
+++ src/player/gamer/statemachine/eggplant/HeuristicEvaluator.java
@@ -0,0 +1,19 @@
+package player.gamer.statemachine.eggplant;
+
+import player.gamer.statemachine.StateMachineGamer;
+import util.statemachine.MachineState;
+import util.statemachine.Move;
+import util.statemachine.Role;
+import util.statemachine.StateMachine;
+import util.statemachine.exceptions.GoalDefinitionException;
+import util.statemachine.exceptions.MoveDefinitionException;
+import util.statemachine.exceptions.TransitionDefinitionException;
+import util.statemachine.implementation.prover.cache.CachedProverStateMachine;
+import apps.player.config.ConfigPanel;
+import apps.player.detail.DetailPanel;
+
+public interface HeuristicEvaluator {
+	
+	public int eval(StateMachine machine, MachineState state, Role role);
+	
+}
