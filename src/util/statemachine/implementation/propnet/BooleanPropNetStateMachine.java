8fa4ca61d2ce7849d55deef3904199c8655f3d0a
Frank Li <strategist333@yahoo.com>
2010-05-11 00:23:46 -0700

    Modified goal value calculation to use their helper method

--- src/util/statemachine/implementation/propnet/BooleanPropNetStateMachine.java
+++ src/util/statemachine/implementation/propnet/BooleanPropNetStateMachine.java
@@ -146,7 +146,7 @@ public class BooleanPropNetStateMachine extends StateMachine {
 					throw new GoalDefinitionException(state, role);
 				} else {
 					Log.println('p', "Goal found: " + goal.getName().toSentence().get(1));
-					goalValue = Integer.parseInt(goal.getName().toSentence().get(1).toString());
+					goalValue = getGoalValue(goal);
 					goalFound = true;
 				}
 			}
