b4f4b23f4e620c1fcc94141e5fae5a1db3cc4195
David Fisher <ddfisher@stanford.edu>
2010-05-14 11:19:40 -0700

    Added Convenient Testing Class

---
+++ src/apps/TestRun.java
@@ -0,0 +1,19 @@
+package apps;
+
+import java.io.IOException;
+
+import player.GamePlayer;
+import player.gamer.statemachine.eggplant.EggplantPrimaryGamer;
+import player.gamer.statemachine.reflex.legal.LegalGamer;
+
+public class TestRun {
+	private static final int DEFAULT_GAME_PORT = 9147;
+	public static void main(String[] args) throws IOException {
+		GamePlayer player = new GamePlayer(DEFAULT_GAME_PORT, new EggplantPrimaryGamer());
+		GamePlayer legal = new GamePlayer(DEFAULT_GAME_PORT+1, new LegalGamer());
+		player.start();
+		legal.start();
+		System.out.println("Started Players!");
+	}
+
+}
